5287e3287a88c1de31a533589a7ea635f4da96ae
bungafitriani30 <bungafitriani30@gmail.com>
2026-08-27 22:25:21 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE IF NOT EXISTS db_skindetect;
-
 USE db_skindetect;
+
+ALTER TABLE prediksi
+ADD COLUMN image_url VARCHAR(1000) NULL;
 
 CREATE TABLE IF NOT EXISTS kelas_penyakit (
     id_kelas INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,7 +15,10 @@ VALUES
 (1, 'Chickenpox', 'Hasil klasifikasi mengarah pada Chickenpox atau cacar air.'),
 (2, 'Dermatitis Eksim', 'Hasil klasifikasi mengarah pada Dermatitis atau Eksim.'),
 (3, 'Measles', 'Hasil klasifikasi mengarah pada Measles atau campak.'),
-(4, 'Monkeypox', 'Hasil klasifikasi mengarah pada Monkeypox.');
+(4, 'Monkeypox', 'Hasil klasifikasi mengarah pada Monkeypox.')
+ON DUPLICATE KEY UPDATE
+nama_kelas = VALUES(nama_kelas),
+deskripsi = VALUES(deskripsi);
 
 CREATE TABLE IF NOT EXISTS prediksi (
     id_prediksi INT AUTO_INCREMENT PRIMARY KEY,
